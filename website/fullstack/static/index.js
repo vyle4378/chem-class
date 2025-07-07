9c2696d4361c56aa8e620d598bae08576856434e
vyle4378 <vyle4378@gmail.com>
2025-07-06 17:08:41 -0700

    removed old files

--- website/fullstack/static/index.js
+++ website/fullstack/static/index.js
@@ -1,18 +1,42 @@
-import { chemistryCurriculum } from "/static/curriculum.js";
+const newButton = document.getElementById('newButton')
 
-const curriculumContainer = document.getElementById("curriculumContainer");
+newButton.addEventListener("click", async () => {
+    try {
+        const response = await fetch('/new-item', {method: "POST"})
+        const item = await response.json()
 
-function createCurriculum() {
-    for (const item of chemistryCurriculum.items) {
-        const itemElement = document.createElement("div");
-        itemElement.classList.add("curriculum-item");
-        itemElement.innerHTML= `<button>${item.title}</button>`
-        
-        itemElement.addEventListener("click", () => {
-            window.location.href = `/item/${item.tag}`
-        });
-        curriculumContainer.appendChild(itemElement);
+        window.location.href = `/edit-page/${item.title}`
+    } catch(e) {
+        console.error("Error:", e)
+    }
+})
+
+async function getTitles() {
+    try {
+        const response = await fetch('/titles')
+        const titles = await response.json()
+        // console.log(titles)
+
+        for (const title of titles) {
+            const itemElement = document.createElement("div");
+            itemElement.classList.add("curriculum-item");
+
+            // itemElement.innerHTML= "<a href= ">${title}</button>`
+            // curriculumContainer.append(itemElement)
+
+            // itemElement.addEventListener("click", () => {
+            //     window.location.href = `/item-page/${title}`
+
+            itemElement.innerHTML= `<button>${title}</button>`
+            curriculumContainer.append(itemElement)
+
+            itemElement.addEventListener("click", () => {
+                window.location.href = `/item-page/${title}`
+            })
+        }
+    } catch(e) {
+        console.error("Error:", e)
     }
 }
 
-createCurriculum();
+getTitles()
--- website/fullstack/static/item.js
+++ website/fullstack/static/item.js
@@ -1,21 +1,33 @@
-import { chemistryCurriculum } from "/static/curriculum.js"
-
 const pathParts = window.location.pathname.split('/');
-const tag = pathParts[pathParts.length - 1];
+const title = pathParts[pathParts.length - 1].replace("%20", " ");
 
 const itemTitle = document.getElementById('itemTitle')
 const contentContainer = document.getElementById('contentContainer')
 const editButton = document.getElementById('editButton')
+const deleteButton = document.getElementById('deleteButton')
 
-function generateItemMaterials () {
-    const item = chemistryCurriculum.items.find(item => item.tag === tag)
-    if (item) {
+async function generateItemMaterials () {
+    try {
+        const response = await fetch(`/item/${title}`)
+        const item = await response.json()
+
         itemTitle.innerHTML = item.title
-        editButton.addEventListener("click", () => {
-            window.location.href = `/edit/${item.tag}`
-        })
         contentContainer.innerHTML = `<p>${item.content}</p>`
-    }   
+
+        editButton.addEventListener("click", () => {
+            window.location.href = `/edit-page/${title}`
+        })
+
+        deleteButton.addEventListener("click", async () => {
+            const response = await fetch(`/delete-item/${title}`, {method: 'DELETE'})
+            
+            window.location.href = '/'
+            const rem_item = await response.json()
+            console.log(rem_item)
+        })
+    } catch(error) {
+        console.error("Error:", error)
+    }
 }
 
 generateItemMaterials()
